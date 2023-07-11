69f54fc7062650a576b02bade8189bd393285907
Mohammed Ismail <mohammedismail523261@gmail.com>
2023-07-11 10:14:21 +0530

    added inital frontend and backend structure

---
+++ amplify/frontend/index.js
@@ -0,0 +1,9 @@
+import { ThemeProvider } from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
+
+import awsconfig from './aws-exports';
+
+import "@aws-amplify/ui-react/styles.css";
+import { studioTheme } from "./ui-components";
+
+Amplify.configure(awsconfig);
