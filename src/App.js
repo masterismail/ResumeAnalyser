67659deb82a9e3df7aa2c9d801fdcdfab54ef808
Mohammed Ismail <mohammedismail523261@gmail.com>
2023-07-11 18:53:02 +0530

    basic structure of auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "@aws-amplify/ui-react/styles.css";
+import {
+  View,
+  withAuthenticator,
+  Button,
+  Heading,
+  Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
+function App({signOut}){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn karta
-        </a>
-      </header>
-    </div>
-  );
+    <View className="App">
+      <Card>
+        <Heading level={1}>Welcome to Amplify!</Heading>
+        </Card> 
+        <Button onClick={signOut}>Sign out</Button>
+    </View>
+  )
 }
-
-export default App;
+export default withAuthenticator(App);
