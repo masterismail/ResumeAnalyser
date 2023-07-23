eca9144e2ce7f9ad9e64963869bafeabce2adad4
Mohammed Ismail <mohammedismail523261@gmail.com>
2023-07-23 13:59:02 +0530

    ui components added

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,6 +3,10 @@
 
 const awsmobile = {
     "aws_project_region": "eu-north-1",
+    "aws_appsync_graphqlEndpoint": "https://zautc4mdljcmlcm6t43tsopno4.appsync-api.eu-north-1.amazonaws.com/graphql",
+    "aws_appsync_region": "eu-north-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-zfsi53bi2nh45odcx46he3ykba",
     "aws_cognito_identity_pool_id": "eu-north-1:0ef6776b-18e8-43f0-ac8b-f45fa7e844b9",
     "aws_cognito_region": "eu-north-1",
     "aws_user_pools_id": "eu-north-1_opjw6jxBa",
@@ -26,10 +30,6 @@ const awsmobile = {
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
     ],
-    "aws_appsync_graphqlEndpoint": "https://zautc4mdljcmlcm6t43tsopno4.appsync-api.eu-north-1.amazonaws.com/graphql",
-    "aws_appsync_region": "eu-north-1",
-    "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-zfsi53bi2nh45odcx46he3ykba",
     "aws_user_files_s3_bucket": "resumeinsightd4bc960853764939bda3ab68cd6f17cf111503-staging",
     "aws_user_files_s3_bucket_region": "eu-north-1"
 };
